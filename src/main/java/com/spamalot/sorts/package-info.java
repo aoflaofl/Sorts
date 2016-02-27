0bb497322164cf960981c647728a0c84cabcebf5
Johannsen <gene.johannsen@hpe.com>
2016-02-26 18:35:25 -0800

    Checkstyle

--- src/main/java/com/spamalot/sorts/package-info.java
+++ src/main/java/com/spamalot/sorts/package-info.java
@@ -1,6 +1,6 @@
 /**
- * A set of classes to play around with and measure
- * the efficiency of various sorting algorithms.
+ * A set of classes to play around with and measure the efficiency of various
+ * sorting algorithms.
  * 
  * <p>
  * You've seen all this before.
